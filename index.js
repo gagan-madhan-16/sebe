7f1df4c3e6a4d76af51bef9cfe7f470dd0ec779d
Gagan Madhan <158490863+gagan-madhan-16@users.noreply.github.com>
2024-12-05 20:21:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,7 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    console.log('CORS middleware executed');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
-
 app.use(express.json());
-
 
 app.use("/api/v1", rootRouter);
 
